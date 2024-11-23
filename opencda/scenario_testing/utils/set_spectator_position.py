4fcb7f4d37b3a1b3231e5b9fd45a62e0838f70ec
Grigoriy Khoroshilov <101859570+Haralishev77@users.noreply.github.com>
2024-11-23 01:03:52 +0000

    [scenarios] Adjusted map Town06 and added utilities for spectator positioning

---
+++ opencda/scenario_testing/utils/set_spectator_position.py
@@ -0,0 +1,12 @@
+import carla  
+import random  
+  
+client = carla.Client('localhost', 2000)  
+world = client.get_world()  
+  
+spectator = world.get_spectator()  
+  
+x, y, z = map(float, input().split(","))  
+location = carla.Location(x=x, y=y, z=z)  
+rotation = carla.Rotation(pitch=0, yaw=-180, roll=0)  
+spectator.set_transform(carla.Transform(location, rotation))
