f828cb3749a25f450be4e3d3236fdfdb320bd976
Andrey Karpukhin <57502612+AndK17@users.noreply.github.com>
2025-04-12 02:42:01 +0300

    [utils] Better get_spectator_position

--- opencda/scenario_testing/utils/get_spectator_position.py
+++ opencda/scenario_testing/utils/get_spectator_position.py
@@ -3,5 +3,8 @@ import carla
 client = carla.Client('carla', 2000)  
 world = client.get_world()  
 
-spectator = world.get_spectator()  
-print(spectator.get_transform())
+spectator = world.get_spectator()
+location = spectator.get_transform().location
+rotation = spectator.get_transform().rotation
+print(f'Location: {location.x:.2f}, {location.y:.2f}, {location.z:.2f},')
+print(f'Rotation: {rotation.pitch:.2f}, {rotation.yaw:.2f}, {rotation.roll:.2f}')
