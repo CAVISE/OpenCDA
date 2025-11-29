05b7db6187dc195c03bff74065076769c78ccd29
AndK17 <karpukhin_av@mail.ru>
2025-11-29 23:29:47 +0300

    [AIM] Add a unified interface for integrating AIM models with the AIMModelManager

---
+++ AIM/models/__init__.py
@@ -0,0 +1,14 @@
+import pkgutil
+import importlib
+
+
+# Init all subdirectories as modules
+def _discover_model_packages():
+    for info in pkgutil.iter_modules(__path__, prefix="AIM.models."):
+        if not info.ispkg:
+            continue
+
+        importlib.import_module(info.name)
+
+
+_discover_model_packages()
