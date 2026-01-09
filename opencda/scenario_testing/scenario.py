d2f52f190aefb0b5e691d831cb15f65a3172c0f9
Matvey <69459474+FlameTigerFlash@users.noreply.github.com>
2026-01-09 17:07:30 +0300

    Update scenario.py

--- opencda/scenario_testing/scenario.py
+++ opencda/scenario_testing/scenario.py
@@ -139,11 +139,7 @@ class Scenario:
             aim_model_name = aim_config.pop("model", "MTP")
             model = get_model(aim_model_name, **aim_config)
 
-            self.codriving_model_manager = AIMModelManager(
-                model=model,
-                nodes=nodes,
-                excluded_nodes=None
-            )            
+            self.codriving_model_manager = AIMModelManager(model=model, nodes=nodes, excluded_nodes=None)           
         # [CoDrivingInt]
 
         self.platoon_list, self.node_ids["platoon"] = self.scenario_manager.create_platoon_manager(
