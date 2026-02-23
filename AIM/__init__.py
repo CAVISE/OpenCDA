4363460de3c68302983baaf4bf774e347471e09f
Victoria Chelnokova <148325729+tori-da-vi@users.noreply.github.com>
2026-02-23 17:30:34 +0100

    Add type annotations to public API functions
    Added type annotations for get_model and list_models.

--- AIM/__init__.py
+++ AIM/__init__.py
@@ -1,11 +1,22 @@
+"""
+AIM module public API.
+
+Provides:
+- AIMModel base class
+- get_model factory function
+- list_models utility
+"""
+
 import importlib
+from typing import Callable, Iterable
+
 from .aim_model import AIMModel
 from .registry import ModelRegistry
 
-# init models
+# Initialize model discovery
 importlib.import_module("AIM.models")
 
-get_model = ModelRegistry.get_model
-list_models = ModelRegistry.list_models
+get_model: Callable = ModelRegistry.get_model
+list_models: Callable[[], Iterable[str]] = ModelRegistry.list_models
 
 __all__ = ["AIMModel", "get_model", "list_models"]
