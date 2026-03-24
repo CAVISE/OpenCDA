b76287b892e0adbd0c57f4a4307a40954977538a
Victoria Chelnokova <148325729+tori-da-vi@users.noreply.github.com>
2026-03-24 21:06:00 +0100

    Refactor get_model and list_models assignments

--- AIM/__init__.py
+++ AIM/__init__.py
@@ -8,7 +8,6 @@ Provides:
 """
 
 import importlib
-from typing import Callable, Iterable
 
 from .aim_model import AIMModel
 from .registry import ModelRegistry
@@ -16,7 +15,7 @@ from .registry import ModelRegistry
 # Initialize model discovery
 importlib.import_module("AIM.models")
 
-get_model: Callable = ModelRegistry.get_model
-list_models: Callable[[], Iterable[str]] = ModelRegistry.list_models
+get_model = ModelRegistry.get_model
+list_models = ModelRegistry.list_models
 
 __all__ = ["AIMModel", "get_model", "list_models"]
