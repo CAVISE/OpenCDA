925cc54c437f77c8103f435e336e53fded66653b
Victoria Chelnokova <148325729+tori-da-vi@users.noreply.github.com>
2026-03-24 21:05:25 +0100

    Fix missing newline at end of __init__.py

--- AIM/models/__init__.py
+++ AIM/models/__init__.py
@@ -16,6 +16,7 @@ def _discover_model_packages() -> None:
     for info in pkgutil.iter_modules(__path__, prefix="AIM.models."):
         if not info.ispkg:
             continue
+
         importlib.import_module(info.name)
 
 
