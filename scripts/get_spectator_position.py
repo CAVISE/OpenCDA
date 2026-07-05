93573faf68a79fa9c0ca484a3a3813259b02a0d8
Grigoriy Khoroshilov <101859570+Haralishev77@users.noreply.github.com>
2026-07-06 00:57:55 +0300

    [scripts] Add CARLA-to-SUMO map conversion utility scripts (#160)

---
+++ scripts/get_spectator_position.py
@@ -0,0 +1,28 @@
+import argparse
+
+
+def arg_parse() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Print the current CARLA spectator transform.")
+    parser.add_argument("--carla-host", type=str, default="localhost", help="IP address or hostname of the CARLA server (default: 'localhost')")
+    parser.add_argument("--carla-port", "--port", dest="carla_port", type=int, default=2000, help="Port of the CARLA server (default: 2000)")
+    parser.add_argument("--carla-timeout", type=float, default=30.0, help="Timeout of the CARLA server response in seconds (default: 30.0)")
+    return parser.parse_args()
+
+
+def main() -> None:
+    opt = arg_parse()
+    import carla
+
+    client = carla.Client(opt.carla_host, opt.carla_port)
+    client.set_timeout(opt.carla_timeout)
+    world = client.get_world()
+
+    spectator = world.get_spectator()
+    location = spectator.get_transform().location
+    rotation = spectator.get_transform().rotation
+    print(f"Location: {location.x:.2f}, {location.y:.2f}, {location.z:.2f},")
+    print(f"Rotation: {rotation.pitch:.2f}, {rotation.yaw:.2f}, {rotation.roll:.2f}")
+
+
+if __name__ == "__main__":
+    main()
